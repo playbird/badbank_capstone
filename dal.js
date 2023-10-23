cf29d4aff4cdd05cc7b46dbd01a964a2df8191f7
James Christensen <bornonthefirstofmay@gmail.com>
2023-10-23 16:33:03 -0400

    updates mongodb connection string, again

--- dal.js
+++ dal.js
@@ -4,7 +4,7 @@ const password = process.env.MONGODB_PWD;
 const username = process.env.MONGO_USERNAME;
 
 let uri =
-`mongodb+srv://${username}:${password}@cluster0.yloiqev.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`;
+`mongodb+srv://bornonthefirstofmay:CBzdpqLDzXXndj1X@cluster0.yloiqev.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`;
 // let uri =  `mongodb+srv://${username}:${password}@cluster0.yloiqev.mongodb.net/`;
 let db = null;
  
