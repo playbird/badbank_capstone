74c5e11a0e3c13175774218c8e7a0749b550d025
James Christensen <bornonthefirstofmay@gmail.com>
2023-10-24 21:11:15 -0400

    begins adding code for heroku vars, currently debugging

--- public/login.js
+++ public/login.js
@@ -1,87 +1,55 @@
-function Login(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');  
-  const [data, setData] = React.useState('log in')  
+import React from 'react';
 
-  return (
-    <Card
-      bgcolor="secondary"
-      header="Login"
-      status={status}
-      body={show ? 
-        <LoginForm setShow={setShow} setStatus={setStatus}/> :
-        <LoginMsg setShow={setShow} setStatus={setStatus}/>}
-    />
-  ) 
-}
-
-function LoginMsg(props){
-  return(<>
-    <h5>Success</h5>
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={() => props.setShow(true)}>
-        Authenticate again
-    </button>
-  </>);
-}
-
-function LoginForm(props){
-  const [email, setEmail]       = React.useState('');
+function LoginForm(props) {
+  const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
 
-  function handle(){
-    fetch(`/account/login/${email}/${password}`)
-    .then(response => response.text())
-    .then(text => {
-        try {
-            const data = JSON.parse(text);
-            props.setStatus('');
-            props.setShow(false);
-            console.log('JSON:', data);
-            setData(data);
-        } catch(err) {
-            // props.setStatus(text)
-            console.log('err:', text);
-        }
-    });
+  async function handle() {
+    // Access Firebase authentication
+    const auth = firebase.auth();
+
+    try {
+      // Sign in with email and password
+      await auth.signInWithEmailAndPassword(email, password);
+
+      // Get the currently signed-in user
+      const user = auth.currentUser;
+
+      // Handle success here, e.g., you can access user for the authenticated user.
+      console.log('User authenticated:', user);
+
+      props.setStatus('Login successful');
+      props.setShow(false);
+    } catch (error) {
+      props.setStatus('Error: ' + error.message);
+    }
   }
 
+  return (
+    <>
+      Email<br />
+      <input
+        type="input"
+        className="form-control"
+        placeholder="Enter email"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      /><br />
 
-  return (<>
+      Password<br />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Enter password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      /><br />
 
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} 
-      onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    Password<br/>
-    <input type="password" 
-      className="form-control" 
-      placeholder="Enter password" 
-      value={password} 
-      onChange={e => setPassword(e.currentTarget.value)}/><br/>
-
-    <button type="submit" className="btn btn-light" onClick={handle}>Login</button>
-   
-  </>);
+      <button type="submit" className="btn btn-light" onClick={handle}>
+        Login
+      </button>
+    </>
+  );
 }
 
-function activeLogin(activeEmail) {
-  const [current, setCurrent] = React.useState('');
-  setCurrent(activeEmail);
-  // console.log(data.email);
-  // console.log(activeEmail);
-  //   do {  
-  //     if (data[i].email == activeEmail) {
-  //       console.log(data[i].email);
-  //       console.log(activeEmail);
-  //       setCurrent(activeEmail);
-  //       return
-  //       }
-  //     i++; 
-  //   }
-  //   while (i < data.length);
-}
+export default LoginForm;
